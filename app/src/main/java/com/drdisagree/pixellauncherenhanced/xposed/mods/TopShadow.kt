be5a9c62e57f79ed467e445344282c11e4b88d81
Raman <81064836+cybr47@users.noreply.github.com>
2026-01-17 17:27:30 +0530

    top shadow fix?
    idk man, im just copy pasting bullshit from the multiple sites.

--- app/src/main/java/com/drdisagree/pixellauncherenhanced/xposed/mods/TopShadow.kt
+++ app/src/main/java/com/drdisagree/pixellauncherenhanced/xposed/mods/TopShadow.kt
@@ -1,9 +1,10 @@
 package com.drdisagree.pixellauncherenhanced.xposed.mods
 
 import android.content.Context
+import android.graphics.Color
+import androidx.core.graphics.ColorUtils
 import com.drdisagree.pixellauncherenhanced.data.common.Constants.LAUNCHER_HIDE_TOP_SHADOW
 import com.drdisagree.pixellauncherenhanced.xposed.ModPack
-import com.drdisagree.pixellauncherenhanced.xposed.mods.LauncherUtils.Companion.restartLauncher
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.XposedHook.Companion.findClass
 import com.drdisagree.pixellauncherenhanced.xposed.mods.toolkit.hookMethod
 import com.drdisagree.pixellauncherenhanced.xposed.utils.XPrefs.Xprefs
@@ -11,24 +12,28 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
 class TopShadow(context: Context) : ModPack(context) {
 
-    private var removeTopShadow = false
+    private var hideTopShadow = false
 
     override fun updatePrefs(vararg key: String) {
         Xprefs.apply {
-            removeTopShadow = getBoolean(LAUNCHER_HIDE_TOP_SHADOW, false)
+            hideTopShadow = getBoolean(LAUNCHER_HIDE_TOP_SHADOW, false)
         }
-        restartLauncher(mContext)
     }
 
     override fun handleLoadPackage(loadPackageParam: LoadPackageParam) {
-        val sysUiScrimClass =
-            findClass("com.android.launcher3.graphics.SysUiScrim")
 
-        sysUiScrimClass
-            .hookMethod("draw")
-            .runBefore { param ->
-                if (!removeTopShadow) return@runBefore
-                param.result = null
+        val allAppsStateClass =
+            findClass("com.android.launcher3.uioverrides.states.AllAppsState")
+
+        allAppsStateClass
+            .hookMethod("getWorkspaceScrimColor")
+            .runAfter { param ->
+                if (!hideTopShadow) return@runAfter
+
+                param.result = when (val res = param.result) {
+                    is Int -> Color.TRANSPARENT
+                    else -> res // fallback safety
+                }
             }
     }
 }
